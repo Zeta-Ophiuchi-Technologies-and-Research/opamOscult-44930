a6a00288d38dbd00d7416cc7c81590f65317ec39
Anshu Naagar <108215633+andydowsen@users.noreply.github.com>
2026-08-17 17:01:00 +0000

    Frontend v1: Atmost ready

---
+++ webapp/webapp/.next/dev/static/chunks/components_operations-map_tsx_0dhmnyw._.js
@@ -0,0 +1,19 @@
+(globalThis["TURBOPACK"] || (globalThis["TURBOPACK"] = [])).push([typeof document === "object" ? document.currentScript : undefined,
+"[project]/components/operations-map.tsx [app-client] (ecmascript, next/dynamic entry, async loader)", ((__turbopack_context__) => {
+
+__turbopack_context__.v((parentImport) => {
+    return Promise.all([
+  {
+    "path": "static/chunks/node_modules_leaflet_dist_leaflet_1ybdq6v.css",
+    "included": [
+      "[project]/node_modules/leaflet/dist/leaflet.css [app-client] (css)"
+    ]
+  },
+  "static/chunks/node_modules_0662r_z._.js",
+  "static/chunks/components_operations-map_tsx_1pef3y3._.js"
+].map((chunk) => __turbopack_context__.l(chunk))).then(() => {
+        return parentImport("[project]/components/operations-map.tsx [app-client] (ecmascript, next/dynamic entry)");
+    });
+});
+}),
+]);
